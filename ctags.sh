3d9adcef66655383300434773e76312264bdb72d
Dan Aloni <alonid@gmail.com>
2018-06-04 12:26:48 +0300

    Git aliases are case insensitive ; Vim updates

--- ctags.sh
+++ ctags.sh
@@ -2,9 +2,11 @@
 # courtesy of http://tbaggery.com/2011/08/08/effortless-ctags-with-git.html
 # modified by https://github.com/richo/git_template/blob/master/hooks/ctags
 
-echo bla > /tmp/xxx
-
 mkdir .git/tags_lock 2>/dev/null || exit 0
 trap "rmdir .git/tags_lock; rm -f .git/tags.$$" EXIT
-git ls-files | ctags --c-kinds=+p --tag-relative -L - -f .git/tags.$$ --exclude=.git --languages=-sql,-css,-javascript
-mv .git/tags.$$ .git/_TAGS_
+grep '^/._TAGS_' .git/info/exclude > /dev/null
+if [[ $? != 0 ]] ; then
+    echo /._TAGS_ >> .git/info/exclude
+fi
+git ls-files | ctags --c-kinds=+p -L - -f .git/tags.$$ --exclude=.git --languages=-sql,-css,-javascript
+mv .git/tags.$$ ._TAGS_
